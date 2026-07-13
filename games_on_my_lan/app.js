f039d67b9d7f22f56d2b673a85b40b78f550da52
Alonso333-RED <alonsitoar018@gmail.com>
2026-07-12 20:56:01 -0400

    Working on a simple landing page

--- games_on_my_lan/app.js
+++ games_on_my_lan/app.js
@@ -2,8 +2,6 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
-import testRoute from "./routes/test.js";
-
 const app = express();
 const PORT = 3000;
 
@@ -12,7 +10,6 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/api/test", testRoute);
 
 app.listen(PORT, () => {
     console.log(`GamesOnMyLan funcionando en puerto ${PORT}`);
